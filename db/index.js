aa1e03883555d430312353f111c55dccf46aade0
carlmenke <carlmenke13@gmail.com>
2022-09-14 21:54:53 -0700

    changed the db index to adjust to heroku

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/egdb' )
+.connect(process.env.MONGO_PROD_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,   })
 .then(()=>{
     console.log('successfully connected to egdb')
 })
@@ -13,3 +16,22 @@ mongoose
 const db = mongoose.connection;
 
 module.exports = db
+
+
+
+
+// const mongoose = require('mongoose')
+
+// mongoose
+// .connect('mongodb://127.0.0.1:27017/egdb' )
+// .then(()=>{
+//     console.log('successfully connected to egdb')
+// })
+// .catch((e)=>{
+//     console.log('Error Connecting: ' , e.message);
+// })
+
+// // mongoose.set('debug' , true)
+// const db = mongoose.connection;
+
+// module.exports = db
