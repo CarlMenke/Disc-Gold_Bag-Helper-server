d8184be49e5e9b8f597f7f9a1d44becebe8a7ee5
carlmenke <carlmenke13@gmail.com>
2022-09-14 22:26:09 -0700

    attempted to connect to mongo db server

--- db/index.js
+++ db/index.js
@@ -1,16 +1,33 @@
-const mongoose = require('mongoose')
 
-mongoose
-.connect('mongodb+srv://carlmenke13:dgb@dgb.n9z04.mongodb.net/sample_mflix?retryWrites=true&w=majority')
-.then(()=>{
-    console.log('successfully connected to egdb')
-})
-.catch((e)=>{
-    console.log('Error Connecting: ' , e.message);
-})
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://dgb:dgb@dgb-database.cnmbjgn.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+// const mongoose = require('mongoose')
+
+// mongoose
+// .connect('mongodb+srv://carl:dgb@dgb.n9z04.mongodb.net/sample_mflix?retryWrites=true&w=majority')
+// .then(()=>{
+//     console.log('successfully connected to egdb')
+// })
+// .catch((e)=>{
+//     console.log('Error Connecting: ' , e.message);
+// })
 
 // mongoose.set('debug' , true)
-const db = mongoose.connection;
+const db = client.connection;
 
 module.exports = db
 
