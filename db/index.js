6034050cb853acd026ca9def974d4d6c76b55433
carlmenke <carlmenke13@gmail.com>
2022-09-14 21:58:46 -0700

    adjusted db index for heroku

--- db/index.js
+++ db/index.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-.connect(process.env.MONGO_PROD_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,   })
+.connect('https://dgb-server.herokuapp.com/api' )
 .then(()=>{
     console.log('successfully connected to egdb')
 })
@@ -20,18 +17,3 @@ module.exports = db
 
 
 
-// const mongoose = require('mongoose')
-
-// mongoose
-// .connect('mongodb://127.0.0.1:27017/egdb' )
-// .then(()=>{
-//     console.log('successfully connected to egdb')
-// })
-// .catch((e)=>{
-//     console.log('Error Connecting: ' , e.message);
-// })
-
-// // mongoose.set('debug' , true)
-// const db = mongoose.connection;
-
-// module.exports = db
